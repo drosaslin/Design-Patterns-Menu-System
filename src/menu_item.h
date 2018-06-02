11d2608e7a1eaa262a2dad9af4ae7e360dc2218a
104590049 <gyauxd@gmail.com>
2018-06-02 14:26:54 +0800

    added isvegetarian and description variables. Added getIngredients,addDescription,  and isVegetarian function

--- src/menu_item.h
+++ src/menu_item.h
@@ -7,8 +7,10 @@
 
 class MenuItem : public BaseMenu {
 public:
-  MenuItem(std::string newName) : BaseMenu(newName)
+  MenuItem(std::string newName, std::string description = "no description available", bool isVegetarian = false) : BaseMenu(newName)
   {
+      _description = description;
+      _isVegetarian = isVegetarian;
   }
 
   void addIngredient(std::string newIngredient) {
@@ -23,16 +25,36 @@ public:
     return false;
   }
 
+  bool isVegetarian (){
+    return _isVegetarian;
+  }
+
+  void addDescription(std::string newDescription){
+    _description = newDescription;
+  }
+
+  std::string getDescription(){
+    return _description;
+  }
+
+  void getIngredients(){
+    for(int n = 0; n < _ingredients.size(); n++){
+      std::cout << _ingredients[n] << std::endl;
+    }
+  }
+
   void removeIngredient(std::string ingredient) {
     int size = _ingredients.size();
     for (int n = 0; n < size; n++){
-      if (_ingredients[n] == ingredient) 
+      if (_ingredients[n] == ingredient)
         _ingredients.erase(_ingredients.begin() + n);
     }
   }
 
 private:
   std::vector<std::string> _ingredients;
+  std::string _description;
+  bool _isVegetarian;
 };
 
 #endif
