82e46e670f71ea0b635bfead64579f77bfce3125
Li-Chun-Chen <ahm12345@gmail.com>
2018-06-03 10:19:39 +0800

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
+#include <cstdlib>
 #include <string>
+#include <stdlib.h>
 using namespace std;
+
+void LoginMenu();
 
 int main()
 {
-  std::cout<<"Welcome to the menu."<<endl;
-  return 1;
+  LoginMenu();
+  string input;
+  while(cin>>input)
+  {
+    if(input=="1")
+    {
+      system("clear");
+      cout<<"You are a Customer."<<endl;
+      //break;
+    }
+    else if(input=="2")
+    {
+      system("clear");
+      cout<<"You are a Manager."<<endl;
+      //break;
+    }
+    else
+    {
+      system("clear");
+      cout<<"Invalid input.Please input'1' or '2'"<<endl;
+    }
+    LoginMenu();
+  }
+
+  return 0;
+}
+
+void LoginMenu()
+{
+  cout<<"\t++++++++++++++++++++++++++++++++++++++++"<<endl
+      <<"\t+                                      +"<<endl
+      <<"\t+     Welcome to the MenuSystem        +"<<endl
+      <<"\t+     1. Customer                      +"<<endl
+      <<"\t+     2. Manager                       +"<<endl
+      <<"\t+                                      +"<<endl
+      <<"\t++++++++++++++++++++++++++++++++++++++++"<<endl;
 }
